c4bd3aef7d886ffef8776f80ff02d02c9cb8a6b6
Daniel O'Connor <daniel.oconnor@gmail.com>
2013-02-11 13:04:15 +1030

    Add IE10, fix comparison to be numeric, not string based

--- lib/browser.rb
+++ lib/browser.rb
@@ -213,7 +213,7 @@ class Browser
 
   # Return true if browser supports some CSS 3 (Safari, Firefox, Opera & IE7+).
   def capable?
-    webkit? || firefox? || opera? || (ie? && version >= "7")
+    webkit? || firefox? || opera? || (ie? && version.to_f >= 7)
   end
 
   def compatibility_view?
@@ -308,6 +308,11 @@ class Browser
   # Detect if browser is Internet Explorer 9.
   def ie9?
     ie? && version == "9"
+  end
+
+  # Detect if browser is Internet Explorer 10.
+  def ie10?
+    ie? && version == "10"
   end
 
   # Detect if browser is running from PSP.
