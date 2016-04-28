b7c7169067b61978ccf84aed255a8aa606909a53
James Carr <carrgo001@gmail.com>
2016-04-28 12:39:40 +1000

    Detect Windows version.

--- lib/browser/platform/windows.rb
+++ lib/browser/platform/windows.rb
@@ -2,8 +2,24 @@
 module Browser
   class Platform
     class Windows < Base
+      VERSION_MAP = {
+        "10.0" => "10",
+        "6.3"  => "8.1",
+        "6.2"  => "8",
+        "6.1"  => "7",
+        "6.0"  => "Vista",
+        "5.2"  => "XP x64 Edition",
+        "5.1"  => "XP",
+        "5.01" => "2000, Service Pack 1 (SP1)",
+        "5.0"  => "2000",
+        "4.0"  => "NT 4.0",
+        "98"   => "98",
+        "95"   => "95",
+      }.freeze
+
       def version
-        "0"
+        nt_version = ua[/Windows(?:\sNT)?\s*([0-9_\.]+)?/, 1]
+        VERSION_MAP[nt_version] || "0"
       end
 
       def name
--- test/unit/windows_test.rb
+++ test/unit/windows_test.rb
@@ -7,6 +7,7 @@ class WindowsTest < Minitest::Test
     assert browser.platform.windows_x64?
     refute browser.platform.windows_wow64?
     assert browser.platform.windows_x64_inclusive?
+    assert_equal browser.platform.version, "8"
   end
 
   test "detects windows wow64" do
@@ -14,6 +15,28 @@ class WindowsTest < Minitest::Test
     refute browser.platform.windows_x64?
     assert browser.platform.windows_wow64?
     assert browser.platform.windows_x64_inclusive?
+    assert_equal browser.platform.version, "8.1"
+  end
+
+  test "detects windows_95" do
+    browser = Browser.new(Browser["WINDOWS_95"])
+
+    assert browser.platform.windows?
+    assert_equal browser.platform.version, "95"
+  end
+
+  test "detects windows_2000" do
+    browser = Browser.new(Browser["WINDOWS_2000"])
+
+    assert browser.platform.windows?
+    assert_equal browser.platform.version, "2000"
+  end
+
+  test "detects windows_2000_sp1" do
+    browser = Browser.new(Browser["WINDOWS_2000_SP1"])
+
+    assert browser.platform.windows?
+    assert_equal browser.platform.version, "2000, Service Pack 1 (SP1)"
   end
 
   test "detects windows_xp" do
@@ -21,6 +44,7 @@ class WindowsTest < Minitest::Test
 
     assert browser.platform.windows?
     assert browser.platform.windows_xp?
+    assert_equal browser.platform.version, "XP"
   end
 
   test "detects windows_xp (64-bit)" do
@@ -29,6 +53,7 @@ class WindowsTest < Minitest::Test
     assert browser.platform.windows?
     assert browser.platform.windows_xp?
     assert browser.platform.windows_x64?
+    assert_equal browser.platform.version, "XP x64 Edition"
   end
 
   test "detects windows_vista" do
@@ -36,6 +61,7 @@ class WindowsTest < Minitest::Test
 
     assert browser.platform.windows?
     assert browser.platform.windows_vista?
+    assert_equal browser.platform.version, "Vista"
   end
 
   test "detects windows7" do
@@ -43,6 +69,7 @@ class WindowsTest < Minitest::Test
 
     assert browser.platform.windows?
     assert browser.platform.windows7?
+    assert_equal browser.platform.version, "7"
   end
 
   test "detects windows8" do
@@ -51,6 +78,7 @@ class WindowsTest < Minitest::Test
     assert browser.platform.windows?
     assert browser.platform.windows8?
     refute browser.platform.windows8_1?
+    assert_equal browser.platform.version, "8"
   end
 
   test "detects windows8.1" do
@@ -59,5 +87,14 @@ class WindowsTest < Minitest::Test
     assert browser.platform.windows?
     assert browser.platform.windows8?
     assert browser.platform.windows8_1?
+    assert_equal browser.platform.version, "8.1"
+  end
+
+  test "detects windows10" do
+    browser = Browser.new(Browser["WINDOWS10"])
+
+    assert browser.platform.windows?
+    assert browser.platform.windows10?
+    assert_equal browser.platform.version, "10"
   end
 end
