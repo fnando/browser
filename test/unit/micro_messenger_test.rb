a0bfb396235807e32f68f5469dd47a9e405eb442
Nando Vieira <fnando.vieira@gmail.com>
2016-05-04 12:45:58 -0300

    Improve micro messenger tests.

--- test/unit/micro_messenger_test.rb
+++ test/unit/micro_messenger_test.rb
@@ -4,8 +4,11 @@ require "test_helper"
 class MicroMessengerTest < Minitest::Test
   test "detects micro messenger 6.3.15" do
     browser = Browser.new(Browser["MICRO_MESSENGER"])
+
     assert browser.micro_messenger?
     assert browser.wechat?
+    assert_equal "6.3.15", browser.full_version
     assert_equal "MicroMessenger", browser.name
+    assert_equal :micro_messenger, browser.id
   end
 end
