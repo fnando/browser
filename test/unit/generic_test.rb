fa9e4ba63c6e1f052674da54d6e5e02f398decf3
Nando Vieira <fnando.vieira@gmail.com>
2016-05-04 12:11:22 -0300

    Add some more tests.

--- test/unit/generic_test.rb
+++ test/unit/generic_test.rb
@@ -7,4 +7,16 @@ class GenericTest < Minitest::Test
     assert_equal "0.0", browser.msie_full_version
     assert_equal "0", browser.msie_version
   end
+
+  test "return default compatibility view" do
+    browser = Browser.new("")
+
+    refute browser.compatibility_view?
+  end
+
+  test "return default safari web app mode" do
+    browser = Browser.new("")
+
+    refute browser.safari_webapp_mode?
+  end
 end
