d1eaef55849e6765c1ca5c273a59743ff8d5715b
Benjamin Borowski <ben.borowski@typeoneerror.com>
2016-08-31 12:06:18 -0700

    [BUGFIX] ensures that iOS platform detection uses OS
    string and not Safari browser agent string

--- lib/browser/platform/ios.rb
+++ lib/browser/platform/ios.rb
@@ -3,10 +3,21 @@ module Browser
   class Platform
     class IOS < Base
       MATCHER = /(iPhone|iPad|iPod)/
-      VERSION_MATCHER = /OS ([\d.]+)/
+      VERSION_MATCHER = /OS ((?<major>\d)_(?<minor>\d)_?(?<patch>\d)?)/
 
       def version
-        ua[VERSION_MATCHER, 1] || "0"
+        matches = VERSION_MATCHER.match(ua)
+        if matches
+          versions = [matches[:major]]
+          if matches[:patch]
+            versions.push(matches[:minor], matches[:patch])
+          else
+            versions.push(matches[:minor]) unless matches[:minor] == "0"
+          end
+          versions.join(".")
+        else
+          "0"
+        end
       end
 
       def name
