f8d63bc3e2e67d060e179bd599ec8294af87f541
Kohei Suzuki <eagletmt@gmail.com>
2017-11-27 17:15:31 +0900

    Lazy load ActionController::Base
    Currently, users cannot configure ActionController in initializers when
    they use browser gem because browser gem loads ActionController::Base
    too early.

--- lib/browser/rails.rb
+++ lib/browser/rails.rb
@@ -9,9 +9,11 @@ module Browser
     config.browser = ActiveSupport::OrderedOptions.new
 
     initializer "browser" do
-      ::ActionController::Base.send :include, Browser::ActionController
-      Browser::Middleware::Context.send :include,
-                                        Browser::Middleware::Context::Additions
+      ActiveSupport.on_load(:action_controller) do
+        ::ActionController::Base.send :include, Browser::ActionController
+        Browser::Middleware::Context.send :include,
+                                          Browser::Middleware::Context::Additions
+      end
     end
   end
 end
