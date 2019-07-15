565906d8654f0195b1caef1433e206eae38eb102
Nando Vieira <me@fnando.com>
2019-07-14 22:09:11 -0700

    Add extension to ActionController::Metal instead. (#384)
    This allows using browser with ActionController::API without having to manually include the module.

--- test/rails_test.rb
+++ test/rails_test.rb
@@ -19,4 +19,14 @@ class RailsTest < Minitest::Test
     assert_equal 200, last_response.status
     assert_equal "Chrome:en-US", last_response.body
   end
+
+  test "renders json" do
+    get "/api/pages", {}, "HTTP_USER_AGENT" => Browser["COMMONCRAWL"],
+                          "HTTP_ACCEPT" => "application/json",
+                          "HTTP_ACCEPT_LANGUAGE" => "en-US;q=0.8"
+
+    assert_equal 200, last_response.status
+    assert_equal true, JSON.parse(last_response.body)["isBot"]
+    assert_equal "en-US", JSON.parse(last_response.body)["acceptLanguages"][0]
+  end
 end
