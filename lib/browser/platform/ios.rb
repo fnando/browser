247360c0aad390daed32b2afbd0b351a6fca768e
Nando Vieira <me@fnando.com>
2019-11-26 10:54:00 -0800

    Always match multiple digits.

--- lib/browser/platform/ios.rb
+++ lib/browser/platform/ios.rb
@@ -4,7 +4,7 @@ module Browser
   class Platform
     class IOS < Base
       MATCHER = /(iPhone|iPad|iPod)/.freeze
-      VERSION_MATCHER = /OS ((?<major>\d+)_(?<minor>\d)_?(?<patch>\d)?)/.freeze
+      VERSION_MATCHER = /OS ((?<major>\d+)_(?<minor>\d+)_?(?<patch>\d+)?)/.freeze
 
       def version
         matches = VERSION_MATCHER.match(ua)
