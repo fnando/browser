e555cebc5913b746a4de758280aaf2789b1ab499
Nando Vieira <me@fnando.com>
2019-11-26 11:02:21 -0800

    Update ios.rb

--- lib/browser/platform/ios.rb
+++ lib/browser/platform/ios.rb
@@ -4,7 +4,7 @@ module Browser
   class Platform
     class IOS < Base
       MATCHER = /(iPhone|iPad|iPod)/.freeze
-      VERSION_MATCHER = /OS ((?<major>\d+)_(?<minor>\d+)_?(?<patch>\d+)?)/.freeze
+      VERSION_MATCHER = /OS ((?<major>\d+)_(?<minor>\d+)_?(?<patch>\d+)?)/.freeze  # rubocop:disable Metrics/LineLength
 
       def version
         matches = VERSION_MATCHER.match(ua)
